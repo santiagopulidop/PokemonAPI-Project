d595e49d6f2862b499e7787dbc89e0a6ade756dc
Cristian Restrepo <cristianrestrepo13@gmail.com>
2020-06-02 18:48:01 -0500

    main page initial components generated and marked

--- src/components/Pages/mainHome.js
+++ src/components/Pages/mainHome.js
@@ -1,33 +1,39 @@
-import React, { Component } from 'react';
-import Title from '../pokemons/title'
-import Search from '../pokemons/searchBar'
+import React, { Component } from "react";
+import Title from "../pokemons/title";
+import Search from "../pokemons/searchBar";
+import PokemonContainer from "../pokemons/pokemonContainer";
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            results : undefined
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: undefined,
+      loading: true,
+    };
+  }
 
-componentDidMount =() =>{
-    fetch('http://pokeapi.salestock.net/api/v2/pokemon/')
-    .then(response => response.json())
-    .then(data =>{
-        console.log(data)
+  componentDidMount = () => {
+    fetch("http://pokeapi.salestock.net/api/v2/pokemon/")
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
-            results : data.results
-        })
-    })
-}
-    render() { 
-        return ( 
-            <>
-                <Title/>
-                <Search/>
-            </>
-         );
+          results: data.results,
+          loading: false,
+        });
+      });
+  };
+  render() {
+    if (!this.state.loading) {
+      return (
+        <div className="container-fluid">
+          <Title />
+          <Search />
+          <PokemonContainer data={this.state.results} />
+        </div>
+      );
     }
+    return <div>Loading...</div>;
+  }
 }
- 
+
 export default Home;
--- src/components/pokemons/title.js
+++ src/components/pokemons/title.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Title extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (  
-            <h1 className='text-center'>Simple Pokedex</h1>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1 className="text-center m-5">Pokémon Finder</h1>;
+  }
 }
- 
+
 export default Title;
