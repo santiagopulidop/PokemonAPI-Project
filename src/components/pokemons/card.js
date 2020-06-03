706a3ee23798bb1cd105a075df58955e0ffb275e
Cristian Restrepo <cristianrestrepo13@gmail.com>
2020-06-02 21:31:07 -0500

    Search bar filter created

--- src/components/pokemons/card.js
+++ src/components/pokemons/card.js
@@ -1,10 +1,14 @@
 import React from "react";
-const Card = ({ name, imgUrl }) => {
+const Card = ({ name, imgUrl, id }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={imgUrl} className="card-img-top" alt="..." />
+    <div className="card m-3" id={id} style={{ width: "18rem" }}>
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body text-center">
-        <p className="card-text">{name}</p>
+        <p className="card-text">{name[0].toUpperCase() + name.slice(1)}</p>
       </div>
     </div>
   );
--- src/components/pokemons/pokemonContainer.js
+++ src/components/pokemons/pokemonContainer.js
@@ -5,20 +5,53 @@ class PokemonContainer extends Component {
     super(props);
     this.state = {
       pokemons: [],
+      search: false,
     };
   }
   componentWillMount() {
+    const { data, input } = this.props;
     this.setState({
-      pokemons: this.props.data,
+      pokemons: data,
     });
+    if (input !== undefined) {
+      this.setState({
+        search: true,
+      });
+    }
   }
   render() {
-    const { pokemons } = this.state;
+    const { pokemons, search } = this.state;
+    const { input } = this.props;
+    if (search) {
+      return (
+        <div className="row p-3 d-flex justify-content-around">
+          {pokemons
+            .filter((pokemon) => {
+              return pokemon.name.includes(input);
+            })
+            .map((value, index) => {
+              return (
+                <Card
+                  id={index + 1}
+                  key={index}
+                  imgUrl={value.url}
+                  name={value.name}
+                />
+              );
+            })}
+        </div>
+      );
+    }
     return (
       <div className="row p-3 d-flex justify-content-around">
         {pokemons.map((value, index) => {
           return (
-            <Card id={index} key={index} imgUrl={value.url} name={value.name} />
+            <Card
+              id={index + 1}
+              key={index}
+              imgUrl={value.url}
+              name={value.name}
+            />
           );
         })}
       </div>
